d8df8003fd7fceed7fd628d507810bbcab41b895
MagicalKarp74 <145033793+MagicalKarp74@users.noreply.github.com>
2024-09-04 01:55:55 +0000

    done (not very well though)

--- Main.java
+++ Main.java
@@ -2,12 +2,26 @@ public class Main{
 
     public static void main(String[] args) {
 
-        Coins amount = new Coins(41);
-        System.out.println(amount.getMoney() + " cents:" );
+        Coins amount = new Coins(94);
+        System.out.println("\n"+amount.getMoney() + " cents:" );
         System.out.println("Quarter(s) "+amount.quarters());
         System.out.println("Dime(s) "+amount.dimes());
         System.out.println("Nickel(s) "+amount.nickels());
         System.out.println("Penny(s) "+amount.pennys());
+
+        Coins amount2 = new Coins(59);
+        System.out.println("\n"+amount2.getMoney() + " cents:" );
+        System.out.println("Quarter(s) "+amount2.quarters());
+        System.out.println("Dime(s) "+amount2.dimes());
+        System.out.println("Nickel(s) "+amount2.nickels());
+        System.out.println("Penny(s) "+amount2.pennys());
+
+        Coins amount3 = new Coins(19);
+        System.out.println("\n"+amount3.getMoney() + " cents:" );
+        System.out.println("Quarter(s) "+amount3.quarters());
+        System.out.println("Dime(s) "+amount3.dimes());
+        System.out.println("Nickel(s) "+amount3.nickels());
+        System.out.println("Penny(s) "+amount3.pennys());
 
 
 }
